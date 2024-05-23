b548425196f6d9ed644546b77fc0d56ed8441297
sametbaglan <sametbaglan@gonder-al.com>
2024-05-23 13:38:11 +0300

    hgf

--- HotelGuide.Service.ContactInformationService/WeatherForecast.cs
+++ HotelGuide.Service.ContactInformationService/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace HotelGuide.Service.ContactInformationService
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
     }
 }
