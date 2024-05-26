03831afe1df367196c51ca7128144851fa7d43c1
sametbaglan <sametbaglan@gonder-al.com>
2024-05-26 04:32:50 +0300

    ss

---
+++ ReportService.Application/Features/HoteInfoReport/Queries/GetHotelInfoReport/GetHotelInfoReportHandler.cs
@@ -0,0 +1,23 @@
+﻿using HotelGuide.Shared.Bases;
+using HotelGuide.Shared.Dtos.ResponseModel;
+using HotelGuide.Shared.Interfaces.MappingTo.AutoMapper;
+using HotelGuide.Shared.Interfaces.UnitOfWorks;
+using MassTransit.Testing;
+using MediatR;
+
+namespace ReportService.Application.Features.HoteInfoReport.Queries.GetHotelInfoReport
+{
+    public class GetHotelInfoReportHandler : BaseHandler, IRequestHandler<GetHotelInfoReportRequest, ResponseDto<List<GetHotelInfoReportResponse>>>
+    {
+        private readonly IPublishedMessage _publishedMessage;
+        public GetHotelInfoReportHandler(IPublishedMessage publishedMessage, IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork)
+        {
+            this._publishedMessage = publishedMessage;
+        }
+
+        public Task<ResponseDto<List<GetHotelInfoReportResponse>>> Handle(GetHotelInfoReportRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReportService.Application/Features/HoteInfoReport/Queries/GetHotelInfoReport/GetHotelInfoReportRequest.cs
@@ -0,0 +1,11 @@
+﻿using HotelGuide.Shared.Dtos.ResponseModel;
+using MassTransit.Caching;
+using MediatR;
+
+namespace ReportService.Application.Features.HoteInfoReport.Queries.GetHotelInfoReport
+{
+    public class GetHotelInfoReportRequest : IRequest<ResponseDto<List<GetHotelInfoReportResponse>>>
+    {
+        public Guid HotelId { get; set; }
+    }
+}
