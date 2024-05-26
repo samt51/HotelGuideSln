579167968e2a76383b8224e31d70ec7fc8b0a6d9
sametbaglan <sametbaglan@gonder-al.com>
2024-05-26 03:21:00 +0300

    GetHotelManagementByHotelId

---
+++ HotelService.Application/Feature/Hotels/Queries/GetHotelManagement/GetHotelManagementQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using HotelGuide.Shared.Bases;
+using HotelGuide.Shared.Dtos.ResponseModel;
+using HotelGuide.Shared.Interfaces.MappingTo.AutoMapper;
+using HotelGuide.Shared.Interfaces.UnitOfWorks;
+using HotelService.Domain.Entities;
+using MediatR;
+
+namespace HotelService.Application.Feature.Hotels.Queries.GetHotelManagement
+{
+    public class GetHotelManagementQueryHandler : BaseHandler, IRequestHandler<GetHotelManagementQueryRequest, ResponseDto<GetHotelManagementQueryResponse>>
+    {
+        public GetHotelManagementQueryHandler(IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork)
+        {
+        }
+
+        public async Task<ResponseDto<GetHotelManagementQueryResponse>> Handle(GetHotelManagementQueryRequest request, CancellationToken cancellationToken)
+        {
+            var dataIsControll = await unitOfWork.GetReadRepository<Hotel>().GetAsync(x => x.Id == request.HotelId && !x.IsDeleted);
+
+            var mapEntity = mapper.Map<GetHotelManagementQueryResponse, Hotel>(dataIsControll);
+
+            return new ResponseDto<GetHotelManagementQueryResponse>().Success(mapEntity);
+        }
+    }
+}
